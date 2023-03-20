b61a080819d4869a33bff7cb0925a2b170d8bbe3
Saikumarpeddineni <128304781+Saikumarpeddineni@users.noreply.github.com>
2023-03-20 20:23:47 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,51 +1,140 @@
-let timerEl = document.getElementById("timer");
-let quoteDisplayEl = document.getElementById("quoteDisplay");
-let resultEl = document.getElementById("result");
-let quoteInputEl = document.getElementById("quoteInput");
-let submitBtn = document.getElementById("submitBtn");
-let resetBtn = document.getElementById("resetBtn");
-let spinnerEl = document.getElementById("spinner");
+let todoItemsContainer = document.getElementById("todoItemsContainer");
+let addTodoButton = document.getElementById("addTodoButton");
+let saveTodoButton = document.getElementById("saveTodoButton");
 
-let intervalId;
-let time;
-
-function startTimer() {
-    time = 1;
-    intervalId = setInterval(function() {
-        timerEl.textContent = time + " seconds";
-        time = time + 1;
-    }, 1000)
-}
-startTimer();
-
-let options = {
-    method: "GET"
-}
-
-function randomQuote() {
-    fetch("https://apis.ccbp.in/random-quote", options)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(randomText) {
-            spinnerEl.classList.add("d-none");
-            quoteDisplayEl.textContent = randomText["content"];
-        })
-}
-randomQuote();
-submitBtn.addEventListener("click", function() {
-    if (quoteInputEl.value === quoteDisplayEl.textContent) {
-        clearInterval(intervalId);
-        resultEl.textContent = "You typed in " + (time - 1) + " seconds";
+function getTodoListFromLocalStorage() {
+    let stringifiedTodoList = localStorage.getItem("todoList");
+    let parsedTodoList = JSON.parse(stringifiedTodoList);
+    if (parsedTodoList === null) {
+        return [];
     } else {
-        resultEl.textContent = "You typed incorrect sentence";
+        return parsedTodoList;
     }
-});
-resetBtn.addEventListener("click", function() {
-    quoteInputEl.value = "";
-    clearInterval(intervalId);
-    startTimer();
-    randomQuote();
-    spinnerEl.classList.remove("d-none");
-    resultEl.textContent = "";
-})
+}
+
+let todoList = getTodoListFromLocalStorage();
+let todosCount = todoList.length;
+
+saveTodoButton.onclick = function() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+};
+
+function onAddTodo() {
+    let userInputElement = document.getElementById("todoUserInput");
+    let userInputValue = userInputElement.value;
+
+    if (userInputValue === "") {
+        alert("Enter Valid Text");
+        return;
+    }
+
+    todosCount = todosCount + 1;
+
+    let newTodo = {
+        text: userInputValue,
+        uniqueNo: todosCount,
+        isChecked: false
+    };
+    todoList.push(newTodo);
+    createAndAppendTodo(newTodo);
+    userInputElement.value = "";
+}
+
+addTodoButton.onclick = function() {
+    onAddTodo();
+};
+
+function onTodoStatusChange(checkboxId, labelId, todoId) {
+    let checkboxElement = document.getElementById(checkboxId);
+    let labelElement = document.getElementById(labelId);
+    labelElement.classList.toggle("checked");
+
+    let todoObjectIndex = todoList.findIndex(function(eachTodo) {
+        let eachTodoId = "todo" + eachTodo.uniqueNo;
+
+        if (eachTodoId === todoId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    let todoObject = todoList[todoObjectIndex];
+
+    if (todoObject.isChecked === true) {
+        todoObject.isChecked = false;
+    } else {
+        todoObject.isChecked = true;
+    }
+
+}
+
+function onDeleteTodo(todoId) {
+    let todoElement = document.getElementById(todoId);
+    todoItemsContainer.removeChild(todoElement);
+
+    let deleteElementIndex = todoList.findIndex(function(eachTodo) {
+        let eachTodoId = "todo" + eachTodo.uniqueNo;
+        if (eachTodoId === todoId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    todoList.splice(deleteElementIndex, 1);
+}
+
+function createAndAppendTodo(todo) {
+    let todoId = "todo" + todo.uniqueNo;
+    let checkboxId = "checkbox" + todo.uniqueNo;
+    let labelId = "label" + todo.uniqueNo;
+
+    let todoElement = document.createElement("li");
+    todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
+    todoElement.id = todoId;
+    todoItemsContainer.appendChild(todoElement);
+
+    let inputElement = document.createElement("input");
+    inputElement.type = "checkbox";
+    inputElement.id = checkboxId;
+    inputElement.checked = todo.isChecked;
+
+    inputElement.onclick = function() {
+        onTodoStatusChange(checkboxId, labelId, todoId);
+    };
+
+    inputElement.classList.add("checkbox-input");
+    todoElement.appendChild(inputElement);
+
+    let labelContainer = document.createElement("div");
+    labelContainer.classList.add("label-container", "d-flex", "flex-row");
+    todoElement.appendChild(labelContainer);
+
+    let labelElement = document.createElement("label");
+    labelElement.setAttribute("for", checkboxId);
+    labelElement.id = labelId;
+    labelElement.classList.add("checkbox-label");
+    labelElement.textContent = todo.text;
+    if (todo.isChecked === true) {
+        labelElement.classList.add("checked");
+    }
+    labelContainer.appendChild(labelElement);
+
+    let deleteIconContainer = document.createElement("div");
+    deleteIconContainer.classList.add("delete-icon-container");
+    labelContainer.appendChild(deleteIconContainer);
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
+
+    deleteIcon.onclick = function() {
+        onDeleteTodo(todoId);
+    };
+
+    deleteIconContainer.appendChild(deleteIcon);
+}
+
+for (let todo of todoList) {
+    createAndAppendTodo(todo);
+}
